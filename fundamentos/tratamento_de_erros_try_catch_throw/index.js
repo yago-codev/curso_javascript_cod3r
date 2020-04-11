f39ef02051394f2f507c49fabdcabb9dd7a66be7
yago-codev <yagomilano92@gmail.com>
2020-04-11 17:18:04 -0300

    Tratamento de erros com try, catch, throw e finally

---
+++ fundamentos/tratamento_de_erros_try_catch_throw/index.js
@@ -0,0 +1,27 @@
+function tratarErroELancar(erro) {
+  // throw 10 // é possível lançar números dentro do erro
+  // throw true // é possível lançar valores booleanos dentro do erro
+  // throw 'mensagem' // é possível lançar somente uma string dentro do erro
+  // throw {
+  //   nome: erro.name,
+  //   msg: erro.message,
+  //   date: new Date
+  // } // é possível lançar um objeto contendo detalhes minuciosos a respeito do erro.
+  throw new Error("A propriedade name é undefined!");
+}
+
+function imprimirNomeGritado(obj) {
+  try {
+    console.log(obj.name.toUpperCase() + "!!!");
+  } catch (err) {
+    tratarErroELancar(err);
+    // o bloco finally será executado independente do erro ocorrer. se tudo der certo, ele também será executado
+  } finally {
+    console.log(
+      "sentença de código sendo executada apesar do erro ter acontecido!"
+    );
+  }
+}
+
+const obj = { nome: "Roberto" };
+imprimirNomeGritado(obj);
