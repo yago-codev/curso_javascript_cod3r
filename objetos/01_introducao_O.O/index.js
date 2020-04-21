af6e7b1edb557f972577e2a3c775719820926e63
yago-codev <yagomilano92@gmail.com>
2020-04-21 20:45:11 -0300

    Objetos: Introdução ao paradigma O.O

---
+++ objetos/01_introducao_O.O/index.js
@@ -0,0 +1,23 @@
+// CÓDIGO NAO EXECUTAVEL
+
+// Programação Procedural
+processamento(valor1, valor2, valor3);
+
+// Programação Orientada a Objetos
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento() {
+    // ...
+  }
+}
+
+objeto.processamento(); // Foco passou a ser o Objeto
+
+// Princíos Importantes:
+
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança (prototype)
+// 4. Polimorfismo
