6dbd5356b49ce3fa3c5d2a4721adab7e9e522463
yago-codev <yagomilano92@gmail.com>
2020-04-21 18:56:20 -0300

    Funções: desafio função construtora

---
+++ funcoes/22_desafio_funcao_construtora/index.js
@@ -0,0 +1,20 @@
+// class Pessoa {
+//   constructor(nome) {
+//     this.nome = nome;
+//   }
+
+//   falar() {
+//     console.log(`Meu nome é ${this.nome}`);
+//   }
+// }
+
+function Pessoa(nome) {
+  this.nome = nome;
+
+  this.falar = function () {
+    console.log(`Meu nome é ${this.nome}`);
+  }
+}
+
+const p1 = new Pessoa('João');
+p1.falar();
